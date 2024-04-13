024f9d5a51b8484acf9f70fa8b5fe94589950787
Nelson Mecha <nelsonorinamecha@gmail.com>
2024-04-13 18:23:43 +0300

    render commit

--- src/index.js
+++ src/index.js
@@ -1,100 +1,100 @@
 // Your code here
 document.addEventListener("DOMContentLoaded", async(event) => {
-    const films = await movieList()
-    const poster = await movieList()
-   displayMovies(films)
-   viewMovie(poster)
-   displayMovieDetails(films)
-   otherDetails(films)
+  const films = await movieList()
+  const poster = await movieList()
+ displayMovies(films)
+ viewMovie(poster)
+ displayMovieDetails(films)
+ otherDetails(films)
 })
 
 
 // frotend logic
 function displayMovies (films) {
- const film = films.map(movie => {
-        return `
-        <li>
-          <div class="movie" id="${movie.id}">
-            ${movie.title}
-          </div>
-        </li>
-        `
-    })
+const film = films.map(movie => {
+      return `
+      <li>
+        <div class="movie" id="${movie.id}">
+          ${movie.title}
+        </div>
+      </li>
+      `
+  })
 
-    const ul = document.getElementById("films")
-    ul.innerHTML = film
+  const ul = document.getElementById("films")
+  ul.innerHTML = film
 }
 
 
- function viewMovie (poster) {
-    const imageCarrier = document.querySelector("#image-carrier")
-    const div = document.createElement("div")
-    //console.log(imageCarrier);
+function viewMovie (poster) {
+  const imageCarrier = document.querySelector("#image-carrier")
+  const div = document.createElement("div")
+  //console.log(imageCarrier);
 
 
-    const view = document.querySelectorAll(".movie")
-    view.forEach(show => {
-        show.addEventListener("click", (event) => {
-            // console.log(event.target.id)
-            const viewPoster = poster.find((element)=> element.id === event.target.id)
-            div.innerHTML = `
-             <img class="posters" src="${viewPoster.poster}" alt="Movie poster"/>
-            `
-            imageCarrier.appendChild(div)
-        })
-    
-    })
+  const view = document.querySelectorAll(".movie")
+  view.forEach(show => {
+      show.addEventListener("click", (event) => {
+          // console.log(event.target.id)
+          const viewPoster = poster.find((element)=> element.id === event.target.id)
+          div.innerHTML = `
+           <img class="posters" src="${viewPoster.poster}" alt="Movie poster"/>
+          `
+          imageCarrier.appendChild(div)
+      })
+  
+  })
 
- }
-
-
- function displayMovieDetails (films) {
-    const details = document.querySelector("#showing")
-    const carrier = document.createElement("div")
+}
 
 
-    const view = document.querySelectorAll(".movie")
-    view.forEach(show => {
-    show.addEventListener("click", (event)=> {
-       // console.log(event.target.id);
-       const viewDetails = films.find((element) => element.id === event.target.id)
-       carrier.innerHTML = `
-       <div class="card">
-       <div id="title" class="title">${viewDetails.title}</div>
-       <div id="runtime" class="meta">${viewDetails.runtime} minutes</div>
-       <div class="content" id="details">
-         <div class="description">
-           <div id="film-info">${viewDetails.description}</div>
-           <span id="showtime" class="ui label">${viewDetails.showtime}</span>
-           <span id="ticket-num">[X]</span> 
-         </div>
+function displayMovieDetails (films) {
+  const details = document.querySelector("#showing")
+  const carrier = document.createElement("div")
+
+
+  const view = document.querySelectorAll(".movie")
+  view.forEach(show => {
+  show.addEventListener("click", (event)=> {
+     // console.log(event.target.id);
+     const viewDetails = films.find((element) => element.id === event.target.id)
+     carrier.innerHTML = `
+     <div class="card">
+     <div id="title" class="title">${viewDetails.title}</div>
+     <div id="runtime" class="meta">${viewDetails.runtime} minutes</div>
+     <div class="content" id="details">
+       <div class="description">
+         <div id="film-info">${viewDetails.description}</div>
+         <span id="showtime" class="ui label">${viewDetails.showtime}</span>
+         <span id="ticket-num">[X]</span> 
        </div>
-       <div class="extra content">
-         <button id="buy-ticket" class="ui orange button">
-           Buy Ticket
-         </button>
-       </div>
-     </div>       
-       
-       `
-       details.appendChild(carrier)
-    })
-   })
- }
- 
+     </div>
+     <div class="extra content">
+       <button id="buy-ticket" class="ui orange button">
+         Buy Ticket
+       </button>
+     </div>
+   </div>       
+     
+     `
+     details.appendChild(carrier)
+  })
+ })
+}
 
 
 
 
-    //backend logic 
+
+  //backend logic 
 function movieList () {
-    return fetch("http://localhost:3000/films",{
-        method:"GET",
-        headers:{
-            "Content-Type":"application/json",
-            "Accept":"application/json"
-        }
-    })
-    .then(resp => resp.json())
-    .then(data => data)
+  return fetch("https://moviedatabase-g11e.onrender.com/films",{
+      method:"GET",
+      headers:{
+          "Content-Type":"application/json",
+          "Accept":"application/json"
+      }
+  })
+  .then(resp => resp.json())
+  .then(data => data)
 }
